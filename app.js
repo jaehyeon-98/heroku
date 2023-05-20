221c9b31b659560c28f43d9507c99499c516c7c4
jaehyeon-98 <jaehyeon@baejaehyeon-ui-MacBookPro.local>
2023-05-20 17:34:42 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+const db = require("./models/index");
+
+app.get("/api/markers/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(markers);
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening");
+});
