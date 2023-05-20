54114bbe05b40d7bf21a7c2508a95e6ea388430d
jaehyeon-98 <jaehyeon@baejaehyeon-ui-MacBookPro.local>
2023-05-20 20:58:18 +0900

    config2

--- config/config.js
+++ config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "process.env.DB_USERNAME",
-    password: "process.env.DB_PASSWORD",
-    database: "process.env.DB",
-    host: "process.env.HOST",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST,
     dialect: "mysql",
   },
 };
